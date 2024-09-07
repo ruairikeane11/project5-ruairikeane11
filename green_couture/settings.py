1e93938c712a7a792a7c009e8a0afe0c74ff042f
Ruairi Keane <ruairi.rk@icloud.com>
2024-09-07 11:41:55 +0000

    Added confirmation emails

--- green_couture/settings.py
+++ green_couture/settings.py
@@ -191,6 +191,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'green_couture@example.com'
 
 
 # Default primary key field type
