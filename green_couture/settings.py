fb08aaf44e59141a21c6651743b28b9fb63195ee
Ruairi Keane <ruairi.rk@icloud.com>
2024-09-08 11:02:59 +0000

    Remve secret key and set debug

--- green_couture/settings.py
+++ green_couture/settings.py
@@ -22,13 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8s@j&_&q%kyh^%x_!q^t7nt!!o7e8#7&6d7uz4)%77lp2xpdpq'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
-'green-couture11-736490e30356.herokuapp.com','localhost',
+    'localhost',
+    'green-couture11-736490e30356.herokuapp.com',
 ]
 
 
