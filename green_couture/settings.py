ffc2e6ce606a93a0554a61a9322f1e8c55184a7b
Ruairi Keane <ruairi.rk@icloud.com>
2024-09-09 16:07:53 +0000

    Add secret key to env.py

--- green_couture/settings.py
+++ green_couture/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -196,7 +196,7 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000',
     }
 
-    
+
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'green-couture11'
     AWS_S3_REGION_NAME = 'eu-west-1'
