ed018369cfcc6c03a1b8c07bb9bb3ace97c374e6
Ruairi Keane <ruairi.rk@icloud.com>
2024-09-14 16:36:52 +0000

    Change Faq model

--- faq/models.py
+++ faq/models.py
@@ -7,10 +7,8 @@ class Faq(models.Model):
     """
     question = models.CharField(max_length=255, help_text="Please ask us questions")
     answer = models.TextField(help_text="Answer here")
-    created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['-created_on']
         verbose_name = "FAQ"
         verbose_name_plural = "FAQs"
 
