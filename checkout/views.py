0cd240cedb4a80bf5e7547182c74e70f55f6a455
Ruairi Keane <ruairi.rk@icloud.com>
2024-09-16 18:24:59 +0000

    Added if blocks to checkout success template

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 from .forms import OrderForm
@@ -136,26 +135,44 @@ def checkout(request):
     return render(request, template, context)
 
 
-@login_required
 def checkout_success(request, order_number):
     """
-    Handle successful checkouts.
-    Ensure that only the user who made the order can see the order details
+    Handle successful checkouts
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    if order.user_profile:
-        if order.user_profile.user != request.user:
-            messages.error(request, "You do not have permission to view this order.")
-            return redirect('home')
-        
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
 
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
 
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
-        'from_profile': 'profile' in request.path,
     }
 
     return render(request, template, context)
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -29,12 +30,17 @@ def profile(request):
 
     return render(request, template, context)
 
+@login_required
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
+    if order.user_profile.user != request.user:
+        messages.error(request, "You do not have permission to view this order.")
+        return redirect('profile')
+
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}.'
-        f'A confirmation email was sent to {order_email}.'
+        f'A confirmation email was sent to {order.email}.'
     ))
 
     template = 'checkout/checkout_success.html'
